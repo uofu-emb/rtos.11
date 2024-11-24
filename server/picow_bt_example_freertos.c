58da80dc0ee75c31a49fde3bc113eca5f8a2efd3
Ashton Snelgrove <ashton@snelgrove.science>
2024-11-23 20:17:33 -0700

    Connections change logging.

--- server/picow_bt_example_freertos.c
+++ server/picow_bt_example_freertos.c
@@ -34,6 +34,14 @@ static void packet_handler(uint8_t packet_type, uint16_t channel, uint8_t *packe
             gap_local_bd_addr(local_addr);
             printf("BTstack up and running on %s.\n", bd_addr_to_str(local_addr));
             break;
+        case BTSTACK_EVENT_NR_CONNECTIONS_CHANGED:
+            uint8_t num;
+            if (num = btstack_event_nr_connections_changed_get_number_connections(packet)) {
+                printf("CONNECTED %d\n", num);
+            } else {
+                printf("NO CONNECTED\n");
+            }
+            break;
         default:
             break;
     }
